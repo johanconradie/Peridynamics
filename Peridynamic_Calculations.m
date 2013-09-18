fac684dfacafb6af7f0a359d65a74eda13777eab
Johan Conradie <jhconradie@sun.com>
2013-09-18 15:30:19 +0200

    example 1

--- Peridynamic_Calculations.m
+++ Peridynamic_Calculations.m
@@ -3,7 +3,7 @@ clear all;
 
 display 'peridynamics'
 
-% Domain = [ Cell_ID, Cell_size, x_centroid, y_centroid];
+
 
 Cell_ID = [1:1:18]
 
@@ -14,23 +14,43 @@ y_coords = [0:0.125:1.0]'
 Cell_ID = [1:1:18]'
 
 
-for i=1:18
+for i=1:length(Cell_ID)
     
     if(i < 9 )
         Cell_size(i) = (x_coords(3) - x_coords(2)) *(y_coords(3)-y_coords(1));
-        x_centroid(i) = (x_coords(3) + x_coords(2))/2
+        x_centroid(i) = (x_coords(3) + x_coords(2))/2;
         
     end
     
     if( i >8 && i <17)
         Cell_size(i) = (x_coords(4)-x_coords(3))*(y_coords(2)-y_coords(1));
-        x_centroid(i) = (x_coords(4) + x_coords(3))/2
+        x_centroid(i) = (x_coords(4) + x_coords(3))/2;
     end
     
     if(i>=17)
         Cell_size(i) = (x_coords(5)-x_coords(4))*(y_coords(5) - y_coords(1));
-        x_centroid(i) = (x_coords(5) + x_coords(4))/2
+        x_centroid(i) = (x_coords(5) + x_coords(4))/2;
     end
 end
 
+y_centroid(1) = (0.5+0.25)/2;
+y_centroid(2) = (0.25)/2;
+y_centroid(3) = (0.75+0.5)/2;
+y_centroid(4) = (1+0.75)/2;
+y_centroid(5) = y_centroid(3);
+y_centroid(6) = y_centroid(4);
+y_centroid(7) = y_centroid(1);
+y_centroid(8) = y_centroid(2);
+y_centroid(17) = 0.75;
+y_centroid(18) = 0.25; 
+counter = 0;
+for i = 9:16
+    y_centroid(i) = (1+0.875)/2-counter;
+    counter = counter +0.125;
+end
 
+Domain(:,1) = Cell_ID
+Domain(:,2)= Cell_size
+Domain(:,3)= x_centroid
+Domain(:,4)=y_centroid
+ 
